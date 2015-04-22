0021b78b98f593930c396500368aa23c9495995c
avb89 <avb89@ukr.net>
2015-04-22 18:28:54 +0300

    add changes

--- src/com/intropro/academy/obezverkhyi/Main.java
+++ src/com/intropro/academy/obezverkhyi/Main.java
@@ -1,27 +1,19 @@
 package com.intropro.academy.obezverkhyi;
 
+import com.intropro.academy.obezverkhyi.taskAnagram.Anagram;
+
 /**
  * Created by dtv on 22.04.2015.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getAnagram("dfjsghadjghpadfihpg 1241234  fsdafs "));
+        Anagram an = new Anagram();
+
+        System.out.println(an.getRevers("12345abcdefg67"));
 
     }
 
 
 
-    public static String getAnagram(String str){
 
-        char[] arrChar = str.toCharArray();
-        char[] newArrChar = new char[arrChar.length];
-        int count = arrChar.length - 1;
-        for (int i = 0; i < arrChar.length; i++) {
-            newArrChar[i]=arrChar[count - i];
-
-        }
-        str = String.valueOf(newArrChar);
-
-        return str;
-    }
 }
--- src/com/intropro/academy/obezverkhyi/taskAnagram/Anagram.java
+++ src/com/intropro/academy/obezverkhyi/taskAnagram/Anagram.java
@@ -4,4 +4,22 @@ package com.intropro.academy.obezverkhyi.taskAnagram;
  * Created by dtv on 22.04.2015.
  */
 public class Anagram {
+
+    public String getRevers(String str){
+
+        char[] arrChar = str.toCharArray();
+        char[] newArrChar = new char[arrChar.length];
+        int count = arrChar.length - 1;
+        for (int i = 0; i < arrChar.length; i++) {
+            newArrChar[i]=arrChar[count - i];
+
+        }
+        str = String.valueOf(newArrChar);
+
+        return str;
+    }
+
+    public String getAnagram(String str){
+        return str;
+    }
 }
