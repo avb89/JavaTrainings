1802666079fda13e5abcea8ec920bd7af84ba4c1
avb89 <avb89@ukr.net>
2015-04-22 18:18:19 +0300

    add class

--- src/com/intropro/academy/obezverkhyi/Main.java
+++ src/com/intropro/academy/obezverkhyi/Main.java
@@ -4,4 +4,24 @@ package com.intropro.academy.obezverkhyi;
  * Created by dtv on 22.04.2015.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(getAnagram("dfjsghadjghpadfihpg 1241234  fsdafs "));
+
+    }
+
+
+
+    public static String getAnagram(String str){
+
+        char[] arrChar = str.toCharArray();
+        char[] newArrChar = new char[arrChar.length];
+        int count = arrChar.length - 1;
+        for (int i = 0; i < arrChar.length; i++) {
+            newArrChar[i]=arrChar[count - i];
+
+        }
+        str = String.valueOf(newArrChar);
+
+        return str;
+    }
 }
---
+++ src/com/intropro/academy/obezverkhyi/taskAnagram/Anagram.java
@@ -0,0 +1,7 @@
+package com.intropro.academy.obezverkhyi.taskAnagram;
+
+/**
+ * Created by dtv on 22.04.2015.
+ */
+public class Anagram {
+}
