293010bbe987b2c66d8c7e15ad7d9ce80969df1b
avb89 <avb89@ukr.net>
2015-04-23 12:46:27 +0300

    add changes

--- src/com/intropro/academy/obezverkhyi/Main.java
+++ src/com/intropro/academy/obezverkhyi/Main.java
@@ -8,8 +8,6 @@ import com.intropro.academy.obezverkhyi.taskAnagram.Anagram;
 public class Main {
     public static void main(String[] args) {
 
-        String st = "abcd efg hig";
-
         Anagram an = new Anagram();
         System.out.println(an.getAnagram("Hello world"));
 
