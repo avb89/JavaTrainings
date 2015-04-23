62a6e9c1adb7fcf99566a6ae5707a0e991fc6ac2
avb89 <avb89@ukr.net>
2015-04-23 12:47:23 +0300

    add changes

--- src/com/intropro/academy/obezverkhyi/Main.java
+++ src/com/intropro/academy/obezverkhyi/Main.java
@@ -3,7 +3,7 @@ package com.intropro.academy.obezverkhyi;
 import com.intropro.academy.obezverkhyi.taskAnagram.Anagram;
 
 /**
- * Created by dtv on 22.04.2015.
+ * Created by obezverkhyi on 22.04.2015.
  */
 public class Main {
     public static void main(String[] args) {
